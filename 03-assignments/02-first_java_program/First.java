13ba1beea9569c840d2a38a918e002c40d2b5239
Colin Belmar <colbel22@gmail.com>
2022-02-09 20:22:03 -0600

    First Program Assignment

---
+++ 03-assignments/02-first_java_program/First.java
@@ -0,0 +1,9 @@
+// package 03-assignments.02-first_java_program;
+
+public class First {
+    public static void main(String[] args) {
+		System.out.println("My name is Colin Belmar");
+        System.out.println("I am 30 years old");
+        System.out.println("My home town is St.Louis,MO");
+	}
+}
