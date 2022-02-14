db570e8c6d7c09de697bb8ed4a95ff9d16d52da6
Colin Belmar <colbel22@gmail.com>
2022-02-14 14:52:38 -0600

    Zoo Keeper 1

--- 03-assignments/13-zoo_keeper_part_1/ZooKeeperPart1/src/com/belmar/zookeeperpart1/Gorilla.java
+++ 03-assignments/13-zoo_keeper_part_1/ZooKeeperPart1/src/com/belmar/zookeeperpart1/Gorilla.java
@@ -1,5 +1,19 @@
 package com.belmar.zookeeperpart1;
 
-public class Gorilla {
-
+public class Gorilla extends Mammal {
+	
+	public void throwSomething() {
+		System.out.println("The gorilla threw something!");
+		this.energyLevel-=5;
+	}
+	
+	public void eatBananas() {
+		System.out.println("The gorilla had a banana and is very satisfied");
+		this.energyLevel+=10;
+	}
+	
+	public void climb() {
+		System.out.println("The gorilla climbed a tree!");
+		this.energyLevel-=10;
+	}
 }
