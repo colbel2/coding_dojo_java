d1b1c2769f3cf08a1c94c7f1ed98707f899b25be
colbel2 <colbel22@gmail.com>
2022-02-19 22:06:38 -0600

    First Full Stack complete

--- 04-spring/FirstFullStack/src/main/java/com/belmar/firstfullstack/services/MenuItemService.java
+++ 04-spring/FirstFullStack/src/main/java/com/belmar/firstfullstack/services/MenuItemService.java
@@ -9,26 +9,49 @@ import com.belmar.firstfullstack.repo.MenuItemRepo;
 
 @Service
 public class MenuItemService {
-
-	//adding the menu repo as a dependency
-	private final MenuItemRepo menuRepo;
-	
-	//constructor that has the repo injected into it
-	public MenuItemService(MenuItemRepo menuRepo) {
-		this.menuRepo = menuRepo;
-	}
-	
-	//methods that can use the repo to talk to the db
-	//returns all Menu Items
-	public List<MenuItem> allMenuItems(){
-		return (List<MenuItem>)this.menuRepo.findAll();
-	}
-	
-	public MenuItem createMenuItem(MenuItem menuItem) {
-		return this.menuRepo.save(menuItem);
-	}
-	
-	public MenuItem findMenuItem(Long id) {
-		return this.menuRepo.findById(id).orElse(null);
-	}
+	// adding the book repository as a dependency
+    private final MenuItemRepo menuRepo;
+    
+    
+    //constructor that has the repository injected into it (dependency injection) --> initializes the Service with connection to the repository
+    public MenuItemService(MenuItemRepo menuRepo) {
+    	this.menuRepo = menuRepo;
+    }
+    
+    
+    //methods that can use the repository to talk to db
+ // returns all the Menu Items
+    public List<MenuItem> allMenuItems() {
+    	
+        return (List<MenuItem>)this.menuRepo.findAll();
+    }
+    
+    public MenuItem createMenuItem(MenuItem menuItem) {
+    	
+    	return this.menuRepo.save(menuItem);
+    }
+    
+    
+    //retrieves a MenuItem by id
+    public MenuItem findMenuItem(Long id) {
+    	
+    	return this.menuRepo.findById(id).orElse(null);
+    }
+    
+    
+    //update a menuitem given an object
+    public MenuItem updateItem(MenuItem menuItem) {
+    	return this.menuRepo.save(menuItem);
+    }
+    
+    //delete a menuitem by its id
+    public void deleteItem(Long id) {
+    	this.menuRepo.deleteById(id);
+    	
+    }
+    
+    
+    
+    
 }
+
